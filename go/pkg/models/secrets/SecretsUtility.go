b475b53a44c4543e29f85add8bbccdb8aabf6ee0
Nicholas Barrow <nbarrow@inspiretmstech.com>
2024-11-27 20:44:43 -0500

    chore: working on github ci/cd

--- go/pkg/models/secrets/SecretsUtility.go
+++ go/pkg/models/secrets/SecretsUtility.go
@@ -2,27 +2,12 @@ package secrets
 
 import (
 	"github.com/inspire-labs-tms-tech/supavault/pkg/models/secrets/shims"
-	"runtime"
 )
 
-func getShim() shims.SecretShim {
-	os := runtime.GOOS
-	switch os {
-	case "darwin": // macOS
-		return &shims.MacSecretsShim{}
-	case "linux":
-		return &shims.UbuntuSecretsShim{}
-	case "windows":
-		panic("windows secrets shim not implemented")
-	default:
-		panic(os + " secrets shim not implemented")
-	}
-}
-
 func SetSecret(credentials shims.ClientCredentials) error {
-	return getShim().SetSecret(credentials)
+	return shims.GetShim().SetSecret(credentials)
 }
 
 func GetSecret() (shims.ClientCredentials, error) {
-	return getShim().GetSecret()
+	return shims.GetShim().GetSecret()
 }
